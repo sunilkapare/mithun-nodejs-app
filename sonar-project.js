c10039ff2a4ae62ba8344f9a631c2c6cf25cf996
sunilkapare <109739263+sunilkapare@users.noreply.github.com>
2023-01-08 20:00:24 +0530

    sonar chages

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://13.251.77.202:9000/',
+  serverUrl: 'http://3.7.252.132:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': 'squ_5f2b52d72f4f2f7054397246b36ac22e7d1a35a2',
+	    'sonar.login': 'squ_04fe06288245a8df45cf2f36d7b8eb694941f41b',
 	    //'sonar.login': 'admin',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
