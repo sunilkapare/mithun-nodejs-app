13be286de2c0ffa62dbb7aa95fad540a4245e36b
sunilkapare <109739263+sunilkapare@users.noreply.github.com>
2023-01-11 23:11:45 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://3.108.52.158:9000/',
+  serverUrl: 'http://3.110.182.152:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': 'squ_d2c5085a8d4cc0086d323ec26cdc64d2254b812d',
+	    'sonar.login': 'squ_e82f803527a0aa6461efeb1db80604710ebbed98',
 	    'sonar.login': 'admin',
 	    'sonar.password': 'admin1',
             'sonar.projectVersion':'1.0',
